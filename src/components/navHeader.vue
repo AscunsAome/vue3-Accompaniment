0b8ebc86dede247d65e2568cfee4fd8700f20134
AscunsAome <708875507@qq.com>
2025-07-10 09:17:25 +0800

    commit

---
+++ src/components/navHeader.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="header-container">
+    <div class="header-left flex-box">
+      <el-icon class="icon" size="20" @click="store.commit('collapseMenu')"><Fold /></el-icon>
+      <ul class="flex-box">
+        <li 
+          v-for="(item, index) in selectMenu" 
+          :key="item.path"
+          :class="{selected: route.path === item.path}"
+          class="tab flex-box">
+             <el-icon  size="12"><component :is="item.icon" /></el-icon>
+             <router-link class="text flex-dox" :to="{ path :item.path }">
+                 <a >{{item.name}}</a>
+             </router-link>
+            
+             <el-icon  size="12" class="close" @click="closeTab(item, index)"><Close /></el-icon>
+        </li>
+      </ul>
+    </div>
+    <div class="header-right">
+      <el-dropdown>
+      <div class="el-dropdown-link flex-box">
+          <el-avatar
+             src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+          />
+          <p class="user-name">admin</p>
+      </div>
+      <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>Action 1</el-dropdown-item>
+        <el-dropdown-item>Action 2</el-dropdown-item>
+        <el-dropdown-item>Action 3</el-dropdown-item>
+        <el-dropdown-item disabled>Action 4</el-dropdown-item>
+        <el-dropdown-item divided>Action 5</el-dropdown-item>
+      </el-dropdown-menu>
+      </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex'
+import { useRoute, useRouter } from 'vue-router'
+import { Select } from '@element-plus/icons-vue';
+
+const route = useRoute()
+const router = useRouter()
+const store = useStore()
+const selectMenu = computed(() => store.state.menu.selectMenu )
+
+const closeTab = (item, index) => {
+  store.commit('closeMenu', item)
+  //删除判断
+  if(route.path !== item.path){
+    return
+  }
+  const selectMenuData = selectMenu.value
+  if(index === selectMenuData.length){
+    if(!selectMenuData.length){
+        router.push('/')
+    }else{
+      router.push({
+        path: selectMenuData[index - 1].path
+      })
+    }
+  }else{
+    router.push({
+      path: selectMenuData[index].path
+    })
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+a{
+  // height: 100%;
+  color: #333;
+  font-size: 15px;
+}
+.flex-box{
+  display: flex;
+  align-items: center;
+  height: 100%;
+}
+.header-container{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  background-color: #fff;
+  padding-right: 25px;
+  .header-left{
+    height: 100%;
+    .icon{
+      width: 45px;
+      height: 100%;
+    }
+    .icon:hover {
+      background-color: #f5f5f5;
+      cursor: pointer;
+     }
+    .tab{
+      padding: 0 10px;
+      height: 100%;
+      
+      .text {
+        margin: 0 5px;
+      }
+      .close{
+        visibility: hidden;
+      }
+      &.selected {
+        background-color: #f5f5f5;
+        i {
+          color: #409eff;
+        }
+        a {
+          color: #409eff;
+        }
+     }
+    }
+    .tab:hover{
+        background-color: #f5f5f5;
+        .close{
+          visibility: visible;
+          cursor: pointer;
+          color: #000;
+        }
+      }
+  }
+  .header-right{
+    .user-name{
+      margin-left: 10px;
+    }
+  }
+}
+</style>
